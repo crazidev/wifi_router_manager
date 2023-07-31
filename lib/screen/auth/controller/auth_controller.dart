c1ada1cd638f740b7bb097d66828cf5808c02cbd
Crazidev <crazibeatdeveloper@gmail.com>
2023-08-01 00:47:44 +0100

    #

--- lib/screen/auth/controller/auth_controller.dart
+++ lib/screen/auth/controller/auth_controller.dart
@@ -2,22 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:router_manager/core/app_constant.dart';
 import 'package:router_manager/data/api_client.dart';
-import 'package:flutter_curl/flutter_curl.dart';
+import 'package:dio/dio.dart';
 
 class AuthController extends GetxController {
   String token = '';
   String sessionID = '';
   TextEditingController password = TextEditingController();
 
-  Client client = Client(verbose: true, interceptors: []);
-
   fetchToken() {}
 
   login() async {
-    await client.init();
-    final response = await client.send(
-      Request(url: 'http://192.168.0.1/cgi-bin/http.cgi', method: 'POST'),
-    );
+    final dio = Dio();
+    final response = await dio.post('http://192.168.0.1/cgi-bin/http.cgi');
     print(response);
   }
 }
